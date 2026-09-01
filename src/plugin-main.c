0d2ce5121adb1adfe747bcfcb4c9b5c969ae302e
kitfoxc <amazing@live.cn>
2026-09-02 04:30:02 +0800

    Register HTML Now Playing source

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -1,36 +1,17 @@
-/*
-OBS Spotify Plugin
-Copyright (C) 2026 lingeriegoat https://github.com/lingeriegoat
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program. If not, see <https://www.gnu.org/licenses/>
-*/
-
-
 #include <obs-module.h>
-#include "spotify-source.h"
+#include "html-player-source.h"
 
 OBS_DECLARE_MODULE()
-OBS_MODULE_USE_DEFAULT_LOCALE("obs-spotify-overlay-plugin", "en-US")
+OBS_MODULE_USE_DEFAULT_LOCALE("obs-html-player-plugin", "en-US")
 
 bool obs_module_load(void)
 {
-	spotify_source_register();
-	blog(LOG_INFO, "[obs-spotify-overlay] plugin loaded");
-	return true;
+    html_source_register();
+    blog(LOG_INFO, "[obs-html-player] plugin loaded");
+    return true;
 }
 
 void obs_module_unload(void)
 {
-	spotify_source_unregister();
+    html_source_unregister();
 }
